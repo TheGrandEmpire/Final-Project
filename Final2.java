4ffd4d333f85798a761dd5099774cd2788b8e34c
Spencer <rlthomas@brandeis.edu>
2018-04-23 21:08:37 -0400

    Added the beginnings of the flashcard function
    It's quite incomplete -- we still need to add the mouseListener or button that will add the actual flashcard functionality -- but I figured I'd push this before I go to bed. Also, I moved the displayWindow stuff out of the displayButton action listener (since putting it there is seen as bad form) and now it doesn't quite work.

--- Final2.java
+++ Final2.java
@@ -3,6 +3,7 @@ import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
 import java.util.*;
+import java.lang.*;
 import java.lang.RuntimeException.*;
 
 public class Final2 {
@@ -19,7 +20,7 @@ public class Final2 {
     JPanel middle = new JPanel();
     middle.setLayout(new GridLayout(2,0));
     JPanel bottom = new JPanel();
-    bottom.setLayout(new GridLayout(0,2));
+    bottom.setLayout(new GridLayout(0,3));
 
     JTextField searchField = new JTextField();
     top.add(searchField);
@@ -44,8 +45,31 @@ public class Final2 {
     bottom.add(saveButton);
     JButton displayButton = new JButton("Display");
     bottom.add(displayButton);
-    //@TODO add search, flashcard options later
+    JButton flashcardButton = new JButton("Flashcards");
+    bottom.add(flashcardButton);
     content.add(bottom,BorderLayout.PAGE_END);
+
+    JFrame displayWindow = new JFrame("Display");
+    JPanel displayContent = new JPanel();
+    displayContent.setLayout(new GridLayout(0,2)); //@TODO make a scrollList
+    for(HashMap.Entry<String, String> entry : engToFrn.entrySet()) {
+      displayContent.add(new JLabel(entry.getKey().toString())); //displays the english word
+      displayContent.add(new JLabel(entry.getValue().toString())); //displays the corresponding french word
+      //@TODO alphabetize
+    }
+
+    JFrame flashcardWindow = new JFrame("Flashcards");
+    JPanel flashcardContent = new JPanel();
+    java.util.List<String> keys = new ArrayList<String>(engToFrn.keySet());
+    JLabel flashcardLabel = new JLabel();
+    try {
+      String flashcardKey = keys.get((int)(Math.random()*keys.size()));
+      flashcardLabel.setText(flashcardKey);
+    } catch (IndexOutOfBoundsException iOOBE) {
+      flashcardLabel.setText("No words have been stored.");
+    }
+    flashcardContent.add(flashcardLabel);
+    //@TODO: add mouselistener to show translation, next/previous buttons
 
     saveButton.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
@@ -99,20 +123,13 @@ public class Final2 {
 
     displayButton.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
-        JFrame displayWindow = new JFrame();
-        JPanel displayContent = new JPanel();
-        displayContent.setLayout(new GridLayout(0,2)); //@TODO make a scrollList later
-        for (HashMap.Entry<String, String> entry : engToFrn.entrySet()) {
-          displayContent.add(new JLabel(entry.getKey().toString())); //prints the english word
-          displayContent.add(new JLabel(entry.getValue().toString())); //prints the corresponding french word
-          //@TODO alphabetize later
-        }
-
-        displayWindow.setContentPane(displayContent);
-        displayWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        displayWindow.setLocation(700,200);
-        displayWindow.setSize(500,300);
         displayWindow.setVisible(true);
+      }
+    });
+
+    flashcardButton.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        flashcardWindow.setVisible(true);
       }
     });
 
@@ -121,5 +138,17 @@ public class Final2 {
     window.setLocation(100,100);
     window.setSize(800,300);
     window.setVisible(true);
+
+    displayWindow.setContentPane(displayContent);
+    displayWindow.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    displayWindow.setLocation(700,200);
+    displayWindow.setSize(500,300);
+    displayWindow.setVisible(false);
+
+    flashcardWindow.setContentPane(flashcardContent);
+    flashcardWindow.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    flashcardWindow.setLocation(700,200);
+    flashcardWindow.setSize(500,300);
+    flashcardWindow.setVisible(false);
   }
 }
